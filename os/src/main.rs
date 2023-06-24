2aa5350521921b57c80588bead014a73d305a598
wzq20021202 <3033715900@qq.com>
2023-06-24 22:54:57 +0800

    18301169实验4

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,54 +1,8 @@
-
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
-#[macro_use]
-
-mod console;
-mod lang_items;
-mod sbi;
 
 use core::arch::global_asm;
-
-global_asm!(include_str!("entry.asm"));
-fn clear_bss() {
-    extern "C" {
-        fn sbss();
-        fn ebss();
-    }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
-}
-
-#[no_mangle]
-pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext();
-        fn etext();
-        fn srodata();
-        fn erodata();
-        fn sdata();
-        fn edata();
-        fn sbss();
-        fn ebss();
-        fn boot_stack();
-        fn boot_stack_top();
-    }
-    clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
-        "boot_stack [{:#x}, {:#x})",
-        boot_stack as usize, boot_stack_top as usize
-    );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    println!("Hello, world!");
-    panic!("Shutdown machine!");
-}
-#![no_std]
-#![no_main]
-#![feature(panic_info_message)]
 
 #[macro_use]
 mod console;
@@ -56,9 +10,9 @@ mod lang_items;
 mod sbi;
 mod syscall;
 mod trap;
-mod batch;
-
-use core::arch::global_asm;
+mod loader;
+mod config;
+mod task;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -68,14 +22,17 @@ fn clear_bss() {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
 }
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("[Kernel] Hello, world!");
+    println!("[kernel] Hello, world!");
     trap::init();
-    batch::init();
-    batch::run_next_app();
+    loader::load_apps();
+    task::run_first_task();
+    panic!("Unreachable in rust_main!");
 }
